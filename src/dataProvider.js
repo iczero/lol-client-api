471491239e8c7604f4d4145bf0bf3a93ca00171b
iczero <iczero4@gmail.com>
2019-01-20 15:20:46 -0800

    Fix dataProvider error

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -5,22 +5,22 @@ const DATA_PATH = 'gamedata';
 const api = require('./apiProvider');
 
 let data = null;
+// contains promise if getData is already loading things
+let loadingPromise = null;
 
 /**
- * Get data from the League Client
- * Currently returns champions, perks, perkStyles
+ * Load data from disk or the League Client in case of an update
  * @return {Object}
  */
-async function getData() {
-  if (data) return data;
-  data = {};
+async function loadData() {
+  let out = {};
   let build = await api.request('GET', '/system/v1/builds');
   let version = build.version;
   let path = `${DATA_PATH}/${version}/`;
   try {
-    data.champions = JSON.parse(await fsP.readFile(path + 'champions.json'));
-    data.perks = JSON.parse(await fsP.readFile(path + 'perks.json'));
-    data.perkStyles = JSON.parse(await fsP.readFile(path + 'perkStyles.json'));
+    out.champions = JSON.parse(await fsP.readFile(path + 'champions.json'));
+    out.perks = JSON.parse(await fsP.readFile(path + 'perks.json'));
+    out.perkStyles = JSON.parse(await fsP.readFile(path + 'perkStyles.json'));
   } catch (err) {
     let summoner = await api.request('GET', '/lol-summoner/v1/current-summoner');
     let summonerId = summoner.summonerId;
@@ -29,9 +29,9 @@ async function getData() {
       api.request('GET', '/lol-perks/v1/perks'),
       api.request('GET', '/lol-perks/v1/styles')
     ]);
-    data.champions = p[0];
-    data.perks = p[1];
-    data.perkStyles = p[2];
+    out.champions = p[0];
+    out.perks = p[1];
+    out.perkStyles = p[2];
     // run this later
     (async () => {
       try {
@@ -39,11 +39,26 @@ async function getData() {
       } catch (err) {
         // it already exists, do nothing
       }
-      fsP.writeFile(path + 'champions.json', JSON.stringify(data.champions, null, 2));
-      fsP.writeFile(path + 'perks.json', JSON.stringify(data.perks, null, 2));
-      fsP.writeFile(path + 'perkStyles.json', JSON.stringify(data.perkStyles, null, 2));
+      fsP.writeFile(path + 'champions.json', JSON.stringify(out.champions, null, 2));
+      fsP.writeFile(path + 'perks.json', JSON.stringify(out.perks, null, 2));
+      fsP.writeFile(path + 'perkStyles.json', JSON.stringify(out.perkStyles, null, 2));
     })();
   }
+  return out;
+}
+
+/**
+ * Get League of Legends item data
+ * Currently returns champions, perks, perkStyles
+ * @return {Object}
+ */
+async function getData() {
+  if (loadingPromise) return await loadingPromise;
+  if (data) return data;
+  // data isn't loaded yet
+  loadingPromise = loadData();
+  data = await loadingPromise;
+  loadingPromise = null;
   return data;
 }
 
